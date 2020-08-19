3b350012d5f1b9629bc304b8ccf5964ccd59c446
niravhathi <imniravhathi@gmail.com>
2020-08-19 12:45:02 +0530

    Initial Commit

---
+++ WiproCodingExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WiproCodingExercise
+//
+//  Created by Nirav Hathi on 8/19/20.
+//  Copyright © 2020 Nirav Hathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
